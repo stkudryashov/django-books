d0b56a36373575b05e46a11f8ccb7408a52fd93c
stkudryashov <aleks59niki30@gmail.com>
2021-04-03 16:13:48 +0300

    create book model

--- books/store/models.py
+++ books/store/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='стоимость')
